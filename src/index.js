b7114e74d3ba166386056fb608697a28239703f3
Loqman Djefafla <76605464+loqman1235@users.noreply.github.com>
2024-02-03 13:56:47 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const io = new Server(httpServer, {
 let PORT = process.env.PORT || 3002;
 
 // Middlewares
-app.use(cors({ credentials: true, origin: "*"}));
+app.use(cors({ credentials: true, origin: ["https://dazzlr-frontend-5dwb.vercel.app", "https://dazzlr-frontend-5dwb-git-main-tonyvito12-gmailcom.vercel.app", "https://dazzlr-frontend-5dwb-pt25f96ss-tonyvito12-gmailcom.vercel.app"]}));
 app.use(cookieParser());
 app.use(express.json({ limit: "30mb" }));
 
