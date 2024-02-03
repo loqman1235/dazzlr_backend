c52f78c3f9c4ed5151a9efb40cc31a002a8ed6c7
Loqman Djefafla <76605464+loqman1235@users.noreply.github.com>
2024-02-03 15:05:30 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -19,22 +19,26 @@ dotenv.config();
 const app = express();
 const httpServer = http.createServer(app);
 
-
 let PORT = process.env.PORT || 3002;
 
 // Middlewares
-app.use(cors({ credentials: true, origin: ["https://dazzlr-frontend-5dwb.vercel.app", "https://dazzlr-frontend-5dwb-git-main-tonyvito12-gmailcom.vercel.app", "https://dazzlr-frontend-5dwb-pt25f96ss-tonyvito12-gmailcom.vercel.app"]}));
+app.use(cors({
+  credentials: true,
+  origin: ["https://dazzlr-frontend-5dwb.vercel.app", "https://dazzlr-frontend-5dwb-git-main-tonyvito12-gmailcom.vercel.app", "https://dazzlr-frontend-5dwb-pt25f96ss-tonyvito12-gmailcom.vercel.app"],
+}));
 app.use(cookieParser());
 app.use(express.json({ limit: "30mb" }));
 
 const io = new Server(httpServer, {
-  cors: { origin: "https://dazzlr-frontend-5dwb.vercel.app" },
+  cors: {
+    origin: ["https://dazzlr-frontend-5dwb.vercel.app", "https://dazzlr-frontend-5dwb-git-main-tonyvito12-gmailcom.vercel.app", "https://dazzlr-frontend-5dwb-pt25f96ss-tonyvito12-gmailcom.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 app.set("io", io);
 
 let onlineUsers = [];
-
-
 
 const addUser = (userId, socketId) => {
   !onlineUsers.some((user) => user.userId === userId) &&
@@ -52,15 +56,11 @@ const getUser = (userId) => {
 io.on("connection", (socket) => {
   console.log("User Connected");
 
-  // On connection event adds user to the online users array if he doesnt already exists
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
     io.emit("getOnlineUsers", onlineUsers);
   });
 
-
-
-  // Send and get message
   socket.on("sendMessage", ({ convoId, senderId, receiverId, message }) => {
     const user = getUser(receiverId);
     if (user) {
@@ -72,7 +72,6 @@ io.on("connection", (socket) => {
         message,
       });
 
-      // Emit "updateLatestMessage" event to both sender and receiver
       io.to(senderId).to(user.socketId).emit("updateLatestMessage", {
         convoId,
         latestMessage: message,
@@ -87,16 +86,14 @@ io.on("connection", (socket) => {
   });
 });
 
-// Connection
 const dbConnection = async () => {
   try {
     if (!process.env.MONGO_URL) {
       throw new Error("MONGO_URL environment variable is not defined.");
     }
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("MongoDB successfully connected");
 
-    // routes
     app.use("/api/auth", authRoutes);
     app.use("/api/posts", postRoutes);
     app.use("/api/users", userRoutes);
